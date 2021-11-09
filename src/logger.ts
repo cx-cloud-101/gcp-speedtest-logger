1a98a9d811dc6a96681dcdbf14baa54edb259a6d
iryland <93376412+iryland@users.noreply.github.com>
2021-11-09 16:22:20 +0100

    Changed from deprecated tslint to eslint and updated dependencies (#14)
    * Changed from deprecated tslint to eslint and updated dependencies
    
    * Linting when building project
    
    Co-authored-by: Ida Ryland <ida.ryland@ingka.com>

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,10 @@
 export default class Logger {
 
     public log(message: any) {
-        /* tslint:disable:no-console */
         console.log(message);
-        /* tslint:enable:no-console */
     }
 
     public error(message: any) {
-        /* tslint:disable:no-console */
         console.error(message);
-        /* tslint:enable:no-console */
     }
 }
